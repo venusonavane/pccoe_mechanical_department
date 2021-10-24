ae1979f1f4014371df9f8461be707c2202888a0a
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2021-10-24 21:41:03 +0530

    Update faculty.php

--- people/faculty.php
+++ people/faculty.php
@@ -13,6 +13,11 @@
     <link href="../vender/css/navbar.css" rel="stylesheet" />
     <link href="../vender/css/style1.css" rel="stylesheet" />
     <link rel="stylesheet" href="style2.css">
+    <style>
+        html {
+            scroll-behaviour : smooth;
+        }
+    </style>
 </head>
 
 <body>
